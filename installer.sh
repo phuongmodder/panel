5c11ad2545b9231f496aed0ed11dfceac6ea3590
Firenet Philippines <103311919+firedev541@users.noreply.github.com>
2022-04-09 21:58:26 +0800

    Automated update

--- installer.sh
+++ installer.sh
@@ -1,1 +1,8 @@
-test lang ulit ito
+#!/bin/bash#Script Variables
+HOST='209.159.152.66';
+USER='crypticv_top10vpn';
+PASS='@@@@F1r3n3t';
+DBNAME='crypticv_top10vpn';
+PORT_TCP='1199';
+PORT_UDP='56';
+
